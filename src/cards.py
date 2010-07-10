967c2f7f301cc2e418e8b1a0031872f26f8c7d01
Bryce Petrini <bryce.petrini@gmail.com>
2010-07-10 00:38:39 -0500

    Fix deal, tweaked PlayingCard constructor

--- src/cards.py
+++ src/cards.py
@@ -27,12 +27,16 @@ class Hand:
         return self.hand[random.randint(0, self.size()-1)]
 
 class Deck:
-    def __init__(self, cards=[]):
+    def __init__(self, cards=None):
+        if cards == None:
+            cards = []
         self.cards = cards
     def __iter__(self):
         return self.cards.__iter__()
     def __getitem__(self, index):
         return self.cards[index]
+    def __str__(self):
+        return "[{}]".format(":".join(map(str,self.cards)))
 
     def size(self):
         return len(self.cards)
@@ -46,11 +50,13 @@ class Deck:
 
     def shuffle(self):
         random.shuffle(self.cards)
+        return self
 
     def deal(self, dest, num):
         for i in range(0, num):
             for d in dest:
-                d.add(self.draw())
+                card = self.draw()
+                d.add(card)
 
     def draw(self):
         return self.cards.pop(0)
@@ -60,9 +66,13 @@ class PlayingCard(Card):
     value = {"2":"Two","3":"Three","4":"Four","5":"Five","6":"Six",
               "7":"Seven","8":"Eight","9":"Nine","0":"Ten",
               "J":"Jack","Q":"Queen","K":"King","A":"Ace","O":"Joker"}
-    def __init__(self, name,suit,value,rank):
+    def __init__(self, suit,value,rank):
+        if value == "O":
+            name = PlayingCard.value[value]
+        else :
+            name = PlayingCard.value[value] + " of " + \
+                    PlayingCard.suit[suit] + "s"
         super().__init__(name)
-        self.name = name
         self.suit = suit
         self.value = value
         self.rank = rank
@@ -77,18 +87,14 @@ def makePlayingCardDeck(ranking,suits):
             rank += 1
         elif value == "O":
             name = PlayingCard.value[value]
-            card = PlayingCard(name,"N",value,rank) 
+            card = PlayingCard("N",value,rank) 
             cards.append(card)
-            print(card,"added")
-            card = PlayingCard(name,"N",value,rank) 
+            card = PlayingCard("N",value,rank) 
             cards.append(card)
-            print(card,"added")
         else:
             for suit in list(suits):
-                name = PlayingCard.value[value] + " of " + PlayingCard.suit[suit] + "s"
-                card = PlayingCard(name,suit,value,rank) 
+                card = PlayingCard(suit,value,rank) 
                 cards.append(card)
-                print(card,"added")
     deck = Deck(cards) 
     return deck
 
