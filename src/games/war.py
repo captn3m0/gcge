f7ab643e861ae3df71843a108cc83d34193ac004
Greg Morin <gerglion@gmail.com>
2010-07-10 02:43:06 -0500

    Some war stuff.

--- src/games/war.py
+++ src/games/war.py
@@ -4,12 +4,29 @@ from cards import *
 class Game:
     '''Classic game of War'''
 
+    def info():
+        return {'name' : 'War', 'players' : '2'}
+
     def __init__(self,engine,numPlayers):
-        engine.registerPhases("setup play war next lose win".split())
-        engine.setPhase("setup")
-        self.decks = [Deck() for p in range(0,numPlayers)]
+        engine.registerPhases("play war next lose win".split())
+        engine.setPhase("play")
+        self.decks = [Deck() for p in range(0,numPlayers+1)]
         stdDeck().shuffle().deal(self.decks, 27)
-        for p in range(1, numPlayers):
+        for p in range(1, numPlayers+1):
             engine.registerZone('battle',p)
             engine.registerZone('war',p)
             engine.registerDeck(self.decks[p],p)
+        self.numPlayers = numPlayers
+        engine.ended = False
+
+    def play(self,engine):
+        '''Handle the play phase'''
+        def playCard(e=engine):
+
+    def war(self,engine):
+
+    def next(self,engine):
+
+    def lose(self,engine):
+
+    def win(self,engine):
