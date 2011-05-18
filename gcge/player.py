ee83fd440cfa380ce0a2f9d8a9f71a8382eabdea
Ralph Bean <ralph.bean@gmail.com>
2011-05-17 23:49:30 -0400

    Doctest that catches an exception.

--- gcge/player.py
+++ gcge/player.py
@@ -1,5 +1,25 @@
 class Player:
-    def __init__(self,id,name):
+    """ Represents a player in the game.
+
+    >>> import gcge.player
+    >>> import gcge.cards
+    >>> p = gcge.player.Player('some_id', 'Quixote')
+    >>> print( len(p.hand) )
+    0
+    >>> c = gcge.cards.Card('foo')
+    >>> p.giveCard(c)
+    >>> print( len(p.hand) )
+    1
+    >>> p.takeCard(c)
+    >>> print( len(p.hand) )
+    0
+    >>> p.takeCard(c)
+    Traceback (most recent call last):
+    ...
+    ValueError: list.remove(x): x not in list
+    """
+
+    def __init__(self, id, name):
         self.id = id
         self.name = name
         self.hand = []
